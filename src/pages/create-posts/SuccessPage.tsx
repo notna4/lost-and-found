6ee452bae1b7c65916267ab750cea69516b4473f
Cosmin <oroscosmin09@gmail.com>
2023-11-16 10:19:12 +0200

    animations for the end of creat lost

--- src/pages/create-posts/SuccessPage.tsx
+++ src/pages/create-posts/SuccessPage.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import '../../styles.css';
 import { useNavigate } from 'react-router-dom';
 import { PageRoutes } from '../../routes/routes';
+import Lottie from 'react-lottie';
+import done from '../../assets/done.json'
 
 const SuccessPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: done,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
-    return(
-        <div className='success-page'>
-            <h2>🎉🎉🎉🎉🎉 ahah, congrats 🎉🎉🎉🎉🎉</h2>
-            <button className='btn' id='shadow-btn' onClick={() => navigate(PageRoutes.Main)}>Go to main</button>
-        </div>
-    );
+  return(
+    <div className='success-page'>
+      <Lottie options={defaultOptions} height={200} width={200} />
+      <h2>Lost item added</h2>
+      <h3>We will get back to you when someone finds it</h3>
+      <button className='btn' id='shadow-btn' onClick={() => navigate(PageRoutes.Main)}>Done</button>
+    </div>
+  );
 }
 
 export default SuccessPage;
